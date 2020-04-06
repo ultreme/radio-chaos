d7cca82b47ba81216a076f52981651401fc57f42
Manfred Touron <m@42.am>
2020-04-06 02:52:45 +0200

    feat: add socketio support + mixer say command

--- chaos-bot/mixer.go
+++ chaos-bot/mixer.go
@@ -2,103 +2,47 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"os"
-	"time"
+	"os/exec"
 
 	gosocketio "github.com/graarh/golang-socketio"
-	"github.com/graarh/golang-socketio/transport"
 	"github.com/oklog/run"
+	"moul.io/godev"
 )
 
-func sendJoin(c *gosocketio.Client) {
-	log.Println("Acking /join")
-	result, err := c.Ack("/join", Channel{"main"}, time.Second*5)
+func mixerBotCmd() error {
+	sio, err := newSIOClient("mixer")
 	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Println("Ack result to /join: ", result)
+		return err
 	}
-}
+	defer sio.Close()
 
-func mixerBot() error {
-	c, err := gosocketio.Dial(
-		gosocketio.GetUrl("sockethub.moul.io", 443, true),
-		transport.GetDefaultWebsocketTransport(),
-	)
-	if err != nil {
-		return err
-	}
-
-	err = c.On("/message", func(h *gosocketio.Channel, args Message) {
-		log.Printf("--- got chat message: %v", args)
+	sio.c.On("event:broadcast", func(h *gosocketio.Channel, args Message) {
+		if args.Msg == nil || args.Msg.Text == "" {
+			return
+		}
+		var pm ProtocolMsg
+		err := json.Unmarshal([]byte(args.Msg.Text), &pm)
+		if err != nil {
+			sio.logError(err)
+			return
+		}
+		switch pm.Action {
+		case "radiosay":
+			cmd := exec.Command("say", pm.Args[0])
+			err := cmd.Run()
+			if err != nil {
+				sio.logError(err)
+				return
+			}
+		default:
+			log.Printf("unsupported action: %s", godev.JSON(pm))
+		}
 	})
-	if err != nil {
-		return err
-	}
-	err = c.On("event:join", func(h *gosocketio.Channel, args Message) {
-		log.Printf("--- got event:join: %v", args)
-	})
-	if err != nil {
-		return err
-	}
-	err = c.On("/event:join", func(h *gosocketio.Channel, args Message) {
-		log.Printf("--- got /event:join: %v", args)
-	})
-	if err != nil {
-		return err
-	}
-	err = c.On("event:broadcast", func(h *gosocketio.Channel, args Message) {
-		log.Printf("--- got event:broadcast: %v", args)
-	})
-	if err != nil {
-		return err
-	}
-
-	err = c.On(gosocketio.OnDisconnection, func(h *gosocketio.Channel) {
-		log.Fatal("--- disconnected")
-	})
-	if err != nil {
-		return err
-	}
-
-	err = c.On(gosocketio.OnConnection, func(h *gosocketio.Channel) {
-		log.Print("--- connected")
-	})
-	if err != nil {
-		return err
-	}
-
-	/*
-		go sendJoin(c)
-		go sendJoin(c)
-		go sendJoin(c)
-		go sendJoin(c)
-		go sendJoin(c)
-	*/
 
 	var g run.Group
-	{
-		cancel := make(chan struct{})
-		g.Add(func() error {
-			<-cancel
-			return nil
-		}, func(err error) {
-			c.Close()
-			close(cancel)
-		})
-	}
 	g.Add(run.SignalHandler(context.TODO(), os.Interrupt))
-
 	return g.Run()
-}
-
-type Channel struct {
-	Channel string `json:"channel"`
-}
-
-type Message struct {
-	Id      int    `json:"id"`
-	Channel string `json:"channel"`
-	Text    string `json:"text"`
 }
